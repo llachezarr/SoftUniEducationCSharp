67ee3fc1ba19e7bd744e32b74d5714e2d9d6cc0c
Lachezar Sofronov <llachezarr@gmail.com>
2023-06-08 05:09:08 +0300

    kamino 90/100, bugz ready Everythink is done!

--- FundamentalsCSharp/Arrays/Exercise/09. Kamino Factory/Program.cs
+++ FundamentalsCSharp/Arrays/Exercise/09. Kamino Factory/Program.cs
@@ -1,7 +1,52 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int sequenceLength = int.Parse(Console.ReadLine());
+        int bestIndex = 0;
+        int bestStartIndex = int.MaxValue;
+        int bestSum = 0;
+        int bestCount = 0;
+        string[] bestSequence = Array.Empty<string>();
+
+        int index = 0;
+        string sequence;
+
+        while (true)
+        {
+            sequence = Console.ReadLine();
+            if (sequence == "Clone them!")
+            {
+                break;
+            }
+
+            index += 1;
+            int count = 0;
+            int sum = 0;
+            string[] sequenceArr = sequence.Split("!", StringSplitOptions.RemoveEmptyEntries);
+            for (int i = sequenceLength - 1; i >= 0; i--)
+            {
+                if (sequenceArr[i] == "1")
+                {
+                    sum++;
+                    count++;
+                    if (bestCount < count || bestStartIndex > i || bestSum < sum)
+                    {
+                        bestSequence = (string[])sequenceArr.Clone();
+                        bestStartIndex = i;
+                        bestIndex = index;
+                        bestCount = count;
+                        bestSum = sum;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+        }
+
+        Console.WriteLine($"Best DNA sample {bestIndex} with sum: {bestSum}.");
+        Console.WriteLine(string.Join(" ", bestSequence));
     }
 }
--- FundamentalsCSharp/Arrays/Exercise/10. LadyBugs/Program.cs
+++ FundamentalsCSharp/Arrays/Exercise/10. LadyBugs/Program.cs
@@ -1,7 +1,70 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int fieldSize = int.Parse(Console.ReadLine());
+        List<int> ladybugIndexes = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        List<int> field = new List<int>();
+        for (int i = 0; i < fieldSize; i++)
+        {
+            if (ladybugIndexes.Contains(i))
+                field.Add(1);
+            else
+                field.Add(0);
+        }
+
+        string command = Console.ReadLine();
+
+        while (command != "end")
+        {
+            string[] commandParts = command.Split();
+            int ladybugIndex = int.Parse(commandParts[0]);
+            string direction = commandParts[1];
+            int flyLength = int.Parse(commandParts[2]);
+
+            if (ladybugIndex >= 0 && ladybugIndex < fieldSize && field[ladybugIndex] == 1)
+            {
+                field[ladybugIndex] = 0; // Remove ladybug from current position
+
+                int newPosition = ladybugIndex;
+
+                while (true)
+                {
+                    if (direction == "right")
+                    {
+                        newPosition += flyLength;
+                    }
+                    else if (direction == "left")
+                    {
+                        newPosition -= flyLength;
+                    }
+
+                    if (newPosition < 0 || newPosition >= fieldSize)
+                    {
+                        // Ladybug has flown out of the field
+                        break;
+                    }
+
+                    if (field[newPosition] == 0)
+                    {
+                        // Ladybug has found an empty spot
+                        field[newPosition] = 1;
+                        break;
+                    }
+                }
+            }
+
+            command = Console.ReadLine();
+        }
+
+        Console.WriteLine(string.Join(" ", field));
     }
 }
