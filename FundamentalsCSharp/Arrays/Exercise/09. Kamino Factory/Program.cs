b3e38ee5e7a0396d7bbcd2413b823a8f6902b7af
Lachezar Sofronov <llachezarr@gmail.com>
2023-06-08 05:10:52 +0300

    kamino 100/100

--- FundamentalsCSharp/Arrays/Exercise/09. Kamino Factory/Program.cs
+++ FundamentalsCSharp/Arrays/Exercise/09. Kamino Factory/Program.cs
@@ -2,51 +2,59 @@
 {
     static void Main()
     {
-        int sequenceLength = int.Parse(Console.ReadLine());
-        int bestIndex = 0;
-        int bestStartIndex = int.MaxValue;
+        int index = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+
+        int[] bestDNA = new int[index];
+        int bestLength = 0;
+        int bestPosition = 0;
         int bestSum = 0;
-        int bestCount = 0;
-        string[] bestSequence = Array.Empty<string>();
+        int curentDNA = 0, dnaIndex = 1;
 
-        int index = 0;
-        string sequence;
 
-        while (true)
+        while (input != "Clone them!")
         {
-            sequence = Console.ReadLine();
-            if (sequence == "Clone them!")
-            {
-                break;
-            }
+            int position = 0;
+            int count = 0, curentLength = 0;
+            int curentSum = 0;
+            curentDNA++;
 
-            index += 1;
-            int count = 0;
-            int sum = 0;
-            string[] sequenceArr = sequence.Split("!", StringSplitOptions.RemoveEmptyEntries);
-            for (int i = sequenceLength - 1; i >= 0; i--)
+            int[] array = input
+                .Split(separator: "!", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < array.Length; i++)
             {
-                if (sequenceArr[i] == "1")
-                {
-                    sum++;
-                    count++;
-                    if (bestCount < count || bestStartIndex > i || bestSum < sum)
-                    {
-                        bestSequence = (string[])sequenceArr.Clone();
-                        bestStartIndex = i;
-                        bestIndex = index;
-                        bestCount = count;
-                        bestSum = sum;
-                    }
-                }
-                else
+                if (array[i] != 1)
                 {
                     count = 0;
+                    continue;
+                }
+                count++;
+
+                if (count > curentLength)
+                {
+                    curentLength = count;
+                    position = i;
                 }
             }
-        }
+            position = position - curentLength + 1;
 
-        Console.WriteLine($"Best DNA sample {bestIndex} with sum: {bestSum}.");
-        Console.WriteLine(string.Join(" ", bestSequence));
+            curentSum = array.Sum();
+            if (bestLength < curentLength ||
+                bestLength == curentLength && bestPosition > position ||
+                bestLength == curentLength && bestPosition == position && curentSum > bestSum)
+            {
+                bestSum = curentSum;
+                bestPosition = position;
+                bestLength = curentLength;
+                bestDNA = array;
+                dnaIndex = curentDNA;
+            }
+            input = Console.ReadLine();
+        }
+        Console.WriteLine($"Best DNA sample {dnaIndex} with sum: {bestSum}.");
+        Console.WriteLine(string.Join(" ", bestDNA));
     }
 }
