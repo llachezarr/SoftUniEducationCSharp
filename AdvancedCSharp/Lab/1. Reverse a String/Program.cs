9bbf73a3170c561761dd3f944ff0d11599b9639e
Lachezar Sofronov <vkinig@students.softuni.bg>
2024-02-27 09:37:57 +0200

    Added C# Advanced course/ Added firist problems - stacks and queues.

---
+++ AdvancedCSharp/Lab/1. Reverse a String/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+string input = Console.ReadLine();
+
+Stack<char> stack = new Stack<char>();
+
+foreach (char c in input)
+{
+    stack.Push(c);
+}
+
+while (stack.Count > 0)
+{
+    Console.Write(stack.Pop());
+}
