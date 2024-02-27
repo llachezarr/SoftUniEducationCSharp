75d70016c6c47920a602ff84bf902bdcd4309dec
Lachezar Sofronov <vkinig@students.softuni.bg>
2024-02-27 20:00:34 +0200

    some problems Solved!

--- AdvancedCSharp/Lab/6. Supermarket/Program.cs
+++ AdvancedCSharp/Lab/6. Supermarket/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Queue<string> queue = new Queue<string>();
+
+string input;
+
+while ((input = Console.ReadLine()) != "End")
+{
+    if (input == "Paid")
+    {
+        while (queue.Any())
+        {
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+    else
+    {
+        queue.Enqueue(input);
+    }
+}
+Console.WriteLine($"{queue.Count} people remaining.");
