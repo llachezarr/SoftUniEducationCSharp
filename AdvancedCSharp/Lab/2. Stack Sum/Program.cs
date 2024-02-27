ebd3feb9abfba607f83819f41eb6dfaed106e7b2
Lachezar Sofronov <vkinig@students.softuni.bg>
2024-02-27 14:28:36 +0200

    some problems Solved!

--- AdvancedCSharp/Lab/2. Stack Sum/Program.cs
+++ AdvancedCSharp/Lab/2. Stack Sum/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> input = Console
+    .ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+Stack<int> stack = new Stack<int>(input);
+
+string command = Console.ReadLine().ToLower();
+
+while (command != "end")
+{
+    string[] splitted = command.Split(" ");
+
+    if (command.Contains("add"))
+    {
+        int firstValue = int.Parse(splitted[1]);
+        int secondValue = int.Parse(splitted[2]);
+
+        stack.Push(firstValue);
+        stack.Push(secondValue);
+    }
+    else if (command.Contains("remove"))
+    {
+        int n = int.Parse(splitted[1]);
+
+        if (n <= stack.Count) {
+
+        for (int i = 0; i < n; i++)
+        {
+            stack.Pop();
+        }
+    }
+}
+    command = Console.ReadLine().ToLower();
+}
+
+int sum = 0;
+
+while (stack.Count > 0)
+{
+    sum += stack.Pop();
+}
+
+Console.WriteLine($"Sum: {sum}");
--- AdvancedCSharp/Lab/3. Simple Calculator/Program.cs
+++ AdvancedCSharp/Lab/3. Simple Calculator/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] input = Console
+    .ReadLine()
+    .Split();
+
+Stack<string> operations = 
+    new Stack<string>(new Stack<string>(input));
+
+
+while (operations.Count > 1)
+{
+    int firstValue = int.Parse(operations.Pop());
+    string operatorSign = operations.Pop();
+    int secondValue = int.Parse(operations.Pop());
+
+    if (operatorSign == "+")
+    {
+        operations.Push((firstValue + secondValue).ToString());
+    }
+    if (operatorSign == "-")
+    {
+        operations.Push((firstValue - secondValue).ToString());
+    }
+}
+
+Console.WriteLine(operations.Pop());
